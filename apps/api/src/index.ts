86113486b02f56ab1cb9d1387c0b359768d964a2
Sil van Diepen <me@sil.mt>
2026-05-08 18:24:06 +0200

    feat: custom domains on hakobs.com - pietru, app.pietru, pietru-api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -14,6 +14,8 @@ app.use(
   '*',
   cors({
     origin: [
+      'https://app.pietru.hakobs.com',
+      'https://pietru.hakobs.com',
       /^https:\/\/[a-z0-9-]+\.pietru-dashboard\.pages\.dev$/,
       /^https:\/\/[a-z0-9-]+\.pietru-marketing\.pages\.dev$/,
       'http://localhost:5173',
